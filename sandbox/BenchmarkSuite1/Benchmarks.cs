9b49db373028039fb0415259dbb010eb9ca37d62
nuskey8 <nuskey.md@gmail.com>
2026-05-24 18:56:27 +0900

    feat: add float overloads

--- sandbox/BenchmarkSuite1/Benchmarks.cs
+++ sandbox/BenchmarkSuite1/Benchmarks.cs
@@ -11,6 +11,8 @@ public class Benchmarks
     static readonly double[] TestValues = new double[10000];
     static readonly byte[] buffer = new byte[1000];
 
+    static readonly float[] TestValuesF = new float[10000];
+
     [GlobalSetup]
     public void Setup()
     {
@@ -30,7 +32,16 @@ public class Benchmarks
     }
 
     [Benchmark]
-    public void System_Utf8Formatter()
+    public void System_FloatToString()
+    {
+        foreach (var value in TestValuesF)
+        {
+            _ = value.ToString();
+        }
+    }
+
+    [Benchmark]
+    public void System_Double_Utf8Formatter()
     {
         foreach (var value in TestValues)
         {
@@ -39,7 +50,16 @@ public class Benchmarks
     }
 
     [Benchmark]
-    public void Zmij_ToString()
+    public void System_Float_Utf8Formatter()
+    {
+        foreach (var value in TestValuesF)
+        {
+            Utf8Formatter.TryFormat(value, buffer.AsSpan(), out var _);
+        }
+    }
+
+    [Benchmark]
+    public void Zmij_Double_ToString()
     {
         foreach (var value in TestValues)
         {
@@ -48,9 +68,27 @@ public class Benchmarks
     }
 
     [Benchmark]
-    public void Zmij_TryWrite()
+    public void Zmij_Float_ToString()
+    {
+        foreach (var value in TestValuesF)
+        {
+            _ = Zmij.ToString(value);
+        }
+    }
+
+    [Benchmark]
+    public void Zmij_Double_TryWrite()
     {
         foreach (var value in TestValues)
+        {
+            Zmij.TryWrite(value, buffer.AsSpan(), out var _);
+        }
+    }
+
+    [Benchmark]
+    public void Zmij_Float_TryWrite()
+    {
+        foreach (var value in TestValuesF)
         {
             Zmij.TryWrite(value, buffer.AsSpan(), out var _);
         }
